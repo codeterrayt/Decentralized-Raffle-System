019ecaccc45163e583fbdb76b0caa84d983a845a
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-09-28 15:47:28 +0530

    Update main.sol

--- main.sol
+++ main.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract DecentralizedVoting {
+contract LuckyDraw {
 
     uint private NoOfTicketSold = 0;
     uint private MaxNoOfTickets = 2;
